0aa2fc7fc0a3f5b8004baeedd1fbf3d8a65808bb
bogdan demchenko <bogdandemchenko@gmail.com>
2019-07-21 10:33:00 +0300

    implemented firebase utils, included ability to store authenticated users into our firestore

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,14 +3,40 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-    apiKey: "AIzaSyDag_xmffJ3UuazXCHmsG162RgeEw0YxFg",
-    authDomain: "crwn-db-781f5.firebaseapp.com",
-    databaseURL: "https://crwn-db-781f5.firebaseio.com",
-    projectId: "crwn-db-781f5",
+    apiKey: "AIzaSyAnvQqvJ5N88r0K89ilTR8RjXfxwlo5dBg",
+    authDomain: "crwn-db-e06c8.firebaseapp.com",
+    databaseURL: "https://crwn-db-e06c8.firebaseio.com",
+    projectId: "crwn-db-e06c8",
     storageBucket: "",
-    messagingSenderId: "911989346163",
-    appId: "1:911989346163:web:8cdb207c76803930"
-};;
+    messagingSenderId: "1008564352796",
+    appId: "1:1008564352796:web:e11c60289965bc1f"
+};
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+    const snapShop = userRef.get();
+
+    if (!snapShop.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (e) {
+            console.log('error creating user', e.message);
+        }
+    }
+
+    return userRef;
+};
 
 firebase.initializeApp(config);
 
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -2,10 +2,12 @@ import React from 'react';
 
 import './sign-in-and-sign-up.styles.scss';
 import SignIn from "../../components/sign-in";
+import SingUp from '../../components/sign-up';
 
 const SignInAndSignUpPage = () => (
     <div className='sign-in-and-sign-up'>
         <SignIn />
+        <SingUp />
     </div>
 );
 
